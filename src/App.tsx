2c02307a11249927ca767ed607616a68aea549f0
Ignacio Gonzalia <igonzalia@liricus.com.ar>
2025-03-31 19:06:54 -0300

    light and dark themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,58 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import MoonIcon from "./assets/icon-moon.svg";
+import SunIcon from "./assets/icon-sun.svg";
+import colors from "./colors";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const [darkTheme, setDarkTheme] = useState(() => {
+    return (
+      localStorage.getItem("theme") === "dark" ||
+      (!localStorage.getItem("theme") &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    );
+  });
+
+  useEffect(() => {
+    if (darkTheme) {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+  }, [darkTheme]);
+
+  const handleThemeToggle = () => setDarkTheme((prevTheme) => !prevTheme);
+
+  return (
+    <div
+      className="h-dvh w-dvw flex justify-center items-center"
+      style={{
+        backgroundColor: darkTheme
+          ? colors.dark.background
+          : colors.light.background,
+      }}
+    >
+      <button
+        className="p-4 flex flex-row justify-center items-center gap-4 rounded-md shadow-md"
+        style={{
+          backgroundColor: darkTheme
+            ? colors.dark.cardBackground
+            : colors.light.cardBackground,
+          color: darkTheme ? colors.dark.title : colors.light.title,
+        }}
+        onClick={handleThemeToggle}
+      >
+        <p>{darkTheme ? "LIGHT" : "DARK"}</p>
+        <img
+          src={darkTheme ? SunIcon : MoonIcon}
+          alt="Theme toggle"
+          className="h-5 w-5"
+        />
+      </button>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/colors.ts
@@ -0,0 +1,18 @@
+const colors = {
+  light: {
+    background: "#F6F8FF",
+    cardBackground: "#FEFEFE",
+    title: "#2B3442",
+    text: "#4B6A9B",
+    user: "#0079FF",
+  },
+  dark: {
+    background: "#141D2F",
+    cardBackground: "#1E2A47",
+    title: "#FFFFFF",
+    text: "#FFFFFF",
+    user: "#0079FF",
+  },
+};
+
+export default colors;
