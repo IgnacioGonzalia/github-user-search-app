b054dba4bbcd9aaddb8f26b5f97dcf6263074b1f
Ignacio Gonzalia <igonzalia@liricus.com.ar>
2025-04-01 20:15:23 -0300

    fetch data from GitHub API

---
+++ src/utils/githubAPI.ts
@@ -0,0 +1,48 @@
+import axios from "axios";
+
+const BASE_URL = "https://api.github.com/users";
+
+interface GitHubUser {
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  gravatar_id: string;
+  url: string;
+  html_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  events_url: string;
+  received_events_url: string;
+  type: string;
+  site_admin: boolean;
+  name?: string;
+  company?: string;
+  blog?: string;
+  location?: string;
+  email?: string;
+  hireable?: boolean;
+  bio?: string;
+  twitter_username?: string;
+  public_repos: number;
+  public_gists: number;
+  followers: number;
+  following: number;
+  created_at: string;
+  updated_at: string;
+}
+
+export const fetchUser = async (username: string): Promise<GitHubUser> => {
+  try {
+    const response = await axios.get<GitHubUser>(`${BASE_URL}/${username}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching user.", error);
+    throw error;
+  }
+};
